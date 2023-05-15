a749316a7b99dff9179b9d61d3e61e2578e1590b
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2023-05-15 14:36:48 +1000

    fixed sql service and added get and post

--- rest/src/main/java/com/justin/app/rest/controller/ApiControllers.java
+++ rest/src/main/java/com/justin/app/rest/controller/ApiControllers.java
@@ -1,13 +1,35 @@
 package com.justin.app.rest.controller;
 
+import com.justin.app.rest.models.Issue;
+import com.justin.app.rest.repo.IssueRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ApiControllers {
 
-    @GetMapping("/")
+    @Autowired
+    private IssueRepo issueRepo;
+
+    @GetMapping(value = "/")
     public String getPage() {
         return "Welcome";
     }
+
+    @GetMapping(value = "/issues")
+    public List<Issue> getIssues() {
+        return issueRepo.findAll();
+    }
+
+    @PostMapping(value = "/save")
+    public String  saveIssue(@RequestBody Issue issue) {
+        issueRepo.save(issue);
+        return "saved... ";
+    }
+
 }
